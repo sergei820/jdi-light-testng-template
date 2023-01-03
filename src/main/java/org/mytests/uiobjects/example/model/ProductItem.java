0e7db559134afbeae4bcbfdb18bb97c48da7d243
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2023-01-03 15:07:05 +0400

    First test scenario is in progress

---
+++ src/main/java/org/mytests/uiobjects/example/model/ProductItem.java
@@ -0,0 +1,12 @@
+package org.mytests.uiobjects.example.model;
+
+public class ProductItem {
+
+    String name, size, quantity, price;
+
+
+
+
+
+
+}
--- src/main/java/org/mytests/uiobjects/example/site/pages/SearchResultsPage.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/SearchResultsPage.java
@@ -5,9 +5,14 @@ import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 @Url("/search/?text=shirt")
 public class SearchResultsPage extends WebPage {
 
     @Css(".results>h1")
     public static WebElement searchResultsMessage;
+
+    @Css("ul[class='product__listing product__grid']>div>a")
+    public static List<WebElement> searchResults;
 }
