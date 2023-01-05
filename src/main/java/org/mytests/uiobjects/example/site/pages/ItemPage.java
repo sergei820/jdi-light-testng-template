d1b4f0a74168722cf6def5d1b6c253ef44310059
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2023-01-05 14:18:06 +0400

    Test 2 done

--- src/main/java/org/mytests/uiobjects/example/site/pages/ItemPage.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/ItemPage.java
@@ -2,6 +2,7 @@ package org.mytests.uiobjects.example.site.pages;
 
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath;
 import lombok.Data;
 import org.openqa.selenium.WebElement;
 
@@ -13,4 +14,19 @@ public class ItemPage extends WebPage {
 
     @Css(".product-details .price")
     private WebElement itemPrice;
+
+    @Css("a#tabreview")
+    private WebElement reviewsButton;
+
+    @XPath("//button[text()='Write a Review']")
+    private WebElement writeReviewButton;
+
+    @Css("input[name='headline']")
+    private WebElement reviewTitleInput;
+
+    @Css("textarea[name='comment']")
+    private WebElement reviewDescriptionTextarea;
+
+    @Css("div[class='rating-stars js-writeReviewStars']:last-child")
+    private WebElement fiveStarRateElement;
 }
--- src/test/java/org/mytests/tests/steps/ItemPageSteps.java
+++ src/test/java/org/mytests/tests/steps/ItemPageSteps.java
@@ -5,6 +5,7 @@ import org.mytests.uiobjects.example.model.ProductItem;
 import static org.mytests.tests.steps.SearchResultsPageSteps.itemOnSearchResultsPage;
 import static org.mytests.uiobjects.example.model.ProductItemFactory.getItemOnSearchResultsPage;
 import static org.mytests.uiobjects.example.model.ProductItemFactory.getItemParametersOnItemPage;
+import static org.mytests.uiobjects.example.site.ApparelSiteUK.itemPage;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class ItemPageSteps {
@@ -15,5 +16,25 @@ public class ItemPageSteps {
         itemPageProductItem = getItemParametersOnItemPage();
         assertEquals(itemOnSearchResultsPage.getName().substring(0, 24), itemPageProductItem.getName().substring(0, 24));
         assertEquals(itemOnSearchResultsPage.getPrice(), itemPageProductItem.getPrice());
+    }
+
+    public void clickReviews() {
+        itemPage.getReviewsButton().click();
+    }
+
+    public void clickWriteReview() {
+        itemPage.getWriteReviewButton().click();
+    }
+
+    public void fillInReviewTitle(String reviewTitle) {
+        itemPage.getReviewTitleInput().sendKeys(reviewTitle);
+    }
+
+    public void fillInReviewDescription(String reviewDescription) {
+        itemPage.getReviewDescriptionTextarea().sendKeys(reviewDescription);
+    }
+
+    public void rateAsFiveStars() {
+        itemPage.getFiveStarRateElement().click();
     }
 }
